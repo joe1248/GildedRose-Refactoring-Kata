2bf74eca212576da5fd9711768b6bfbf943d5cb5
Joe1248 <josephbarban@gmail.com>
2022-11-19 14:55:03 +0100

    Make basic passing test

--- php/fixtures/texttest_fixture.php
+++ php/fixtures/texttest_fixture.php
@@ -11,15 +11,15 @@ echo 'OMGHAI!' . PHP_EOL;
 
 $items = [
     new Item('+5 Dexterity Vest', 10, 20),
-    new Item('Aged Brie', 2, 0),
-    new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-    // this conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6),
+    // new Item('Aged Brie', 2, 0),
+    // new Item('Elixir of the Mongoose', 5, 7),
+    // new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+    // new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+    // new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+    // new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+    // new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+    // // this conjured item does not work properly yet
+    // new Item('Conjured Mana Cake', 3, 6),
 ];
 
 $app = new GildedRose($items);
@@ -27,6 +27,7 @@ $app = new GildedRose($items);
 $days = 2;
 if (count($argv) > 1) {
     $days = (int) $argv[1];
+    echo " *** CHANGING days to ${days} !!! " . PHP_EOL;
 }
 
 for ($i = 0; $i < $days; $i++) {
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,11 +10,52 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    public function testPerishableItem(): void
     {
-        $items = [new Item('foo', 0, 0)];
+        $items = [new Item('+5 Dexterity Vest', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame('+5 Dexterity Vest', $items[0]->name);
     }
 }
+/*
+-------- day 0 --------
+name, sellIn, quality
++5 Dexterity Vest, 10, 20
+
+-------- day 1 --------
+name, sellIn, quality
++5 Dexterity Vest, 9, 19
+
+-------- day 2 --------
+name, sellIn, quality
++5 Dexterity Vest, 8, 18
+
+-------- day 3 --------
+name, sellIn, quality
++5 Dexterity Vest, 7, 17
+
+-------- day 4 --------
+name, sellIn, quality
++5 Dexterity Vest, 6, 16
+
+-------- day 5 --------
+name, sellIn, quality
++5 Dexterity Vest, 5, 15
+
+-------- day 6 --------
+name, sellIn, quality
++5 Dexterity Vest, 4, 14
+
+-------- day 7 --------
+name, sellIn, quality
++5 Dexterity Vest, 3, 13
+
+-------- day 8 --------
+name, sellIn, quality
++5 Dexterity Vest, 2, 12
+
+-------- day 9 --------
+name, sellIn, quality
++5 Dexterity Vest, 1, 11
+*/
