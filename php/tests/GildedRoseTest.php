7d064705172f09b5d3dfcddee245ab2e3ebc79e5
Joe1248 <josephbarban@gmail.com>
2022-11-19 15:14:59 +0100

    basic test cases

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,52 +10,36 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testPerishableItem(): void
+    /**
+     * @dataProvider providePerishableItemData
+     */
+    public function testPerishableItem(
+        array $itemToday,
+        string $itemTomorrow,
+    ): void
     {
-        $items = [new Item('+5 Dexterity Vest', 0, 0)];
+        $items = [new Item(...$itemToday)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('+5 Dexterity Vest', $items[0]->name);
+        $this->assertSame($itemTomorrow, $items[0]->__toString());
+    }
+
+    /**
+    * @return string[][]
+    */
+    public function providePerishableItemData()
+    {
+        yield 'Quality goes down' => [
+            ['+5 Dexterity Vest', 10, 20],
+            '+5 Dexterity Vest, 9, 19'
+        ];
+        yield 'Once the sell by date has passed, Quality degrades twice as fast' => [
+            ['+5 Dexterity Vest', 0, 10],
+            '+5 Dexterity Vest, -1, 8'
+        ];
+        yield 'The Quality of an item is never negative' => [
+            ['+5 Dexterity Vest', 10, 0],
+            '+5 Dexterity Vest, 9, 0'
+        ];
     }
 }
-/*
--------- day 0 --------
-name, sellIn, quality
-+5 Dexterity Vest, 10, 20
-
--------- day 1 --------
-name, sellIn, quality
-+5 Dexterity Vest, 9, 19
-
--------- day 2 --------
-name, sellIn, quality
-+5 Dexterity Vest, 8, 18
-
--------- day 3 --------
-name, sellIn, quality
-+5 Dexterity Vest, 7, 17
-
--------- day 4 --------
-name, sellIn, quality
-+5 Dexterity Vest, 6, 16
-
--------- day 5 --------
-name, sellIn, quality
-+5 Dexterity Vest, 5, 15
-
--------- day 6 --------
-name, sellIn, quality
-+5 Dexterity Vest, 4, 14
-
--------- day 7 --------
-name, sellIn, quality
-+5 Dexterity Vest, 3, 13
-
--------- day 8 --------
-name, sellIn, quality
-+5 Dexterity Vest, 2, 12
-
--------- day 9 --------
-name, sellIn, quality
-+5 Dexterity Vest, 1, 11
-*/
