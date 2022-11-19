fd01c816f4a9888110e75fccaedefc7a8ecdc387
Joe1248 <josephbarban@gmail.com>
2022-11-19 15:22:33 +0100

    test RewardingItem aged brie

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -11,9 +11,9 @@ use PHPUnit\Framework\TestCase;
 class GildedRoseTest extends TestCase
 {
     /**
-     * @dataProvider providePerishableItemData
+     * @dataProvider provideItemData
      */
-    public function testPerishableItem(
+    public function testUpdateQuality(
         array $itemToday,
         string $itemTomorrow,
     ): void
@@ -27,19 +27,32 @@ class GildedRoseTest extends TestCase
     /**
     * @return string[][]
     */
-    public function providePerishableItemData()
+    public function provideItemData()
     {
-        yield 'Quality goes down' => [
+        yield 'PerishableItem : Quality goes down' => [
             ['+5 Dexterity Vest', 10, 20],
             '+5 Dexterity Vest, 9, 19'
         ];
-        yield 'Once the sell by date has passed, Quality degrades twice as fast' => [
+        yield 'PerishableItem : Once the sell by date has passed, Quality degrades twice as fast' => [
             ['+5 Dexterity Vest', 0, 10],
             '+5 Dexterity Vest, -1, 8'
         ];
-        yield 'The Quality of an item is never negative' => [
+        yield 'PerishableItem: The Quality of an item is never negative' => [
             ['+5 Dexterity Vest', 10, 0],
             '+5 Dexterity Vest, 9, 0'
         ];
+        yield 'RewardingItem: "Aged Brie" actually increases in Quality the older it gets' => [
+            ['Aged Brie', 2, 0],
+            'Aged Brie, 1, 1'
+        ];
+        yield 'RewardingItem: "Aged Brie" quality increases twice as fast after sell-by date' => [
+            ['Aged Brie', 0, 2],
+            'Aged Brie, -1, 4'
+        ];
+        yield 'RewardingItem: "Aged Brie" quality increases until maximum 50' => [
+            ['Aged Brie', -24, 50],
+            'Aged Brie, -25, 50'
+        ];
+        
     }
 }
